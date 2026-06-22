74cbc80ff039878d60d53b430d37a4e7225094d8
spartacusrex <patrickcerri@gmail.com>
2026-06-22 09:17:00 +0100

    add minima api module

--- minimaapi/src/androidTest/java/org/minimarex/minimaapi/ExampleInstrumentedTest.java
+++ minimaapi/src/androidTest/java/org/minimarex/minimaapi/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("org.minimarex.minimaapi", appContext.getPackageName());
+        assertEquals("org.minimarex.minimaapi.test", appContext.getPackageName());
     }
 }
